c258ce1d986619e38dad603b11359cd6b265328a
Cassio C <LccassioL@gmail.com>
2023-09-19 23:56:51 -0300

    change for test

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ const routes = express.Router();
 const carController = new CarController();
 
 // Root route
+
 // routes.get('/', (req, res) => res.json({ Api_status: 'working' }));
 routes.get('/', carController.list);
 routes.post('/', carController.create)
