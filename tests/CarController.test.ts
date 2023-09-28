e2ea540b79013467921b8202cc4d98ff6ae7644c
Cassio C <LccassioL@gmail.com>
2023-09-27 23:37:52 -0300

    change to single quotes

--- tests/CarController.test.ts
+++ tests/CarController.test.ts
@@ -1,22 +1,22 @@
-import superTest from "supertest";
-import Server from "../src/server";
+import superTest from 'supertest';
+import Server from '../src/server';
 
-describe("API E2E Test Suite", () => {
-  test("GET / - should return an empty array", async () => {
-    const response = await superTest(Server).get("/");
+describe('API E2E Test Suite', () => {
+  test('GET / - should return an empty array', async () => {
+    const response = await superTest(Server).get('/');
     const data = JSON.parse(response.text);
     expect(data).toBeInstanceOf(Array);
-  })
+  });
 
-  test("POST / - should create a new car", async () => {
+  test('POST / - should create a new car', async () => {
     const newCar = {
-      "plate": 1,
-      "year": 2023,
-      "model": "GOL",
-      "brand": "VW"
+      'plate': 1,
+      'year': 2023,
+      'model': 'GOL',
+      'brand': 'VW'
     };
-    const response = await superTest(Server).post("/").send(newCar)
-    .set('Content-Type', 'application/json')
+    const response = await superTest(Server).post('/').send(newCar)
+    .set('Content-Type', 'application/json');
 
     const savedCard = JSON.parse(response.text);
     expect(savedCard).toStrictEqual(newCar);
--- tests/CarService.test.ts
+++ tests/CarService.test.ts
@@ -1,27 +1,24 @@
-import { CarService } from "../src/entities/Car/CarService"
-
-
-
-test("It should be create a car", async function () {
+import { CarService } from '../src/entities/Car/CarService';
+test('It should be create a car', async function () {
     const carService = new CarService();
     const car = {
-      "plate": 1,
-      "year": 2023,
-      "model": "GOL",
-      "brand": "VW"
+      'plate': 1,
+      'year': 2023,
+      'model': 'GOL',
+      'brand': 'VW'
     }
   
     const savedCar = carService.create(car);
     expect(savedCar).toStrictEqual(car);
 });
 
-test("It should be list cars", async function () {
+test('It should be list cars', async function () {
   const carService = new CarService();
   const car = {
-    "plate": 1,
-    "year": 2023,
-    "model": "GOL",
-    "brand": "VW"
+    'plate': 1,
+    'year': 2023,
+    'model': 'GOL',
+    'brand': 'VW'
   };
 
   const savedCar = carService.create(car);
